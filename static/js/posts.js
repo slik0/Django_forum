a91906a1c76081c2723bfc646481969df8356d75
Elias <egeorgiadis7@gmail.com>
2022-10-09 01:31:31 -0400

    updated

---
+++ static/js/posts.js
@@ -0,0 +1,10 @@
+////////////////////////////////////
+//// Javascript for post page  ////
+//////////////////////////////////
+
+$(function() {
+    // Executed when Js-menu-icon js clicked //
+    $('.js-menu-icon').click(function() {      
+        $(this).next().toggle();
+    })
+})
